7a8554780d1e1080fdd713f72e4b6d331d6b9252
vjdavid <vjdavid@gmail.com>
2015-08-08 22:36:14 -0700

    moved all code in a closure after this created one controller for display the boject customer

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,15 @@
-var app = angular.module("myapp", []);
+(function() {
+
+  var customer = {
+    name: "Juan",
+    lastName: "Lopez",
+    age: 19
+  };
+
+  var app = angular.module("myapp", []);
+
+  app.controller('StoreController', function() {
+    this.person = customer;
+  });
+
+}) ();
